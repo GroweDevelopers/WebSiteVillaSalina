a0139f296043738024f9e3649dd9705a76db1b9a
Singh-Growe <account@growe.dev>
2026-08-26 12:42:58 +0200

    feat(eccellenza): porta il carosello immagini con swiper/react

---
+++ src/components/ui/ImagesSwiper.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { Swiper, SwiperSlide } from 'swiper/react'
+import type { ImageRef } from '@/types'
+
+type ImagesSwiperProps = {
+  images: ImageRef[]
+  /** Classi aggiuntive sul contenitore, ad esempio `mt-5`. */
+  className?: string
+}
+
+/**
+ * Carosello di immagini della sezione "Eccellenza".
+ * Stessa configurazione di `.imagesSwiper` in swiper.js.
+ */
+export function ImagesSwiper({ images, className }: ImagesSwiperProps) {
+  return (
+    <Swiper
+      className={['imagesSwiper', className].filter(Boolean).join(' ')}
+      centeredSlides
+      loop
+      spaceBetween={30}
+      slidesPerView={1}
+      breakpoints={{
+        768: { slidesPerView: 1.5 },
+        991: { slidesPerView: 2.42 },
+      }}
+    >
+      {images.map((image) => (
+        <SwiperSlide key={image.src}>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src={image.src}
+            alt={image.alt}
+            loading="lazy"
+            style={{ width: '100%', height: 459, objectFit: 'cover' }}
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  )
+}
