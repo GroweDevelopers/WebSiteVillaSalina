54f118640b199ce98fd16e0ce2b14de8be099b40
Singh-Growe <account@growe.dev>
2026-08-26 12:46:30 +0200

    feat(gallery): porta i due blocchi masonry dei piatti

---
+++ src/components/sections/GalleryMasonry.tsx
@@ -0,0 +1,76 @@
+import { galleryDishes } from '@/data/gallery'
+import type { ImageRef } from '@/types'
+
+function PortfolioBox({ image, className }: { image: ImageRef; className?: string }) {
+  return (
+    <div className={['portfolio-box-2', className].filter(Boolean).join(' ')}>
+      <div className="image">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src={image.src} alt={image.alt} loading="lazy" />
+      </div>
+    </div>
+  )
+}
+
+/** I due blocchi masonry della pagina Gallery, dodici piatti in tutto. */
+export function GalleryMasonry() {
+  const { first, second } = galleryDishes
+
+  return (
+    <section className="portfolio-mansonry">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="portfolio-mansonry-main">
+              <div className="row-box">
+                <PortfolioBox image={first.hero} className="t1" />
+              </div>
+              <div className="row-box">
+                <div className="left">
+                  <PortfolioBox image={first.left} />
+                </div>
+                <div className="right">
+                  <PortfolioBox image={first.rightTop} />
+                  <PortfolioBox image={first.rightBottom} />
+                </div>
+              </div>
+              <div className="row-box">
+                <div className="left">
+                  <PortfolioBox image={first.bottomLeft} />
+                </div>
+                <div className="right">
+                  <PortfolioBox image={first.bottomRight} />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-12">
+            <div className="portfolio-mansonry-main">
+              <div className="row-box">
+                <PortfolioBox image={second.hero} className="t1" />
+              </div>
+              <div className="row-box">
+                <div className="right">
+                  <PortfolioBox image={second.rightTop} />
+                  <PortfolioBox image={second.rightBottom} />
+                </div>
+                <div className="left">
+                  <PortfolioBox image={second.left} />
+                </div>
+              </div>
+              <div className="row-box">
+                <div className="left">
+                  <PortfolioBox image={second.bottomLeft} />
+                </div>
+                <div className="right">
+                  <PortfolioBox image={second.bottomRight} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
